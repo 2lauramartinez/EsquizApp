b5c52174389a1f9d967a6de669af6c0b5f3c12ea
Laura Martínez <lauramartinezmira@gmail.com>
2020-06-20 13:11:53 +0200

    Update app.js
    Recibir la información del paciente

--- Servidor/app.js
+++ Servidor/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-// // La información se codifica en JSON
+//La información se codifica en JSON
 app.use("/", express.json({ strict: false, limit:"10MB"})); 
 
 const mysql = require('mysql');
@@ -18,6 +18,23 @@ if (err) {
     console.error("Error conectando a la base de datos:", err);
     process.exit();
  	}
+});
+
+
+app.get('/api/paciente/:id', function (req,res) {
+    var sql = "SELECT id_pac, nombre, apellido, sexo, fecha_nac, user, sip, telefono, dni FROM paciente WHERE id_pac = " + req.params.id;
+	connection.query(sql, function (err, paciente) {
+		if(err) {
+			return res.status(500).send("Error en la base de datos")
+			}
+		else{
+			if(!paciente || paciente.length === 0){ 
+				return res.status(404).send("No se encuentra paciente")
+		}
+		};
+		res.json(paciente[0]); //devolver solo un objeto (no el array)
+    });
+    
 });
 
 app.listen(3000, function () {
